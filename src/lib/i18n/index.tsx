61ba6c3e7383e0761b1786fb8898ee08ff1d1857
nejos97 <nenbajonathan@protonmail.com>
2025-10-26 04:04:58 +0100

    feat: upgrade packages and fix code smells

--- src/lib/i18n/index.tsx
+++ src/lib/i18n/index.tsx
@@ -1,20 +1,23 @@
-import { locale } from 'expo-localization';
+import { getLocales } from 'expo-localization';
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
 import { resources } from './resources';
 export * from './utils';
 
-i18n.use(initReactI18next).init({
-  resources,
-  lng: locale, // TODO: if you are not supporting multiple languages or languages with multiple directions you can set the default value to `en`
-  fallbackLng: 'en',
-  compatibilityJSON: 'v3', // By default React Native projects does not support Intl
+i18n.use(initReactI18next).init(
+  {
+    resources,
+    lng: getLocales()[0].languageCode || 'en', // TODO: if you are not supporting multiple languages or languages with multiple directions you can set the default value to `en`
+    fallbackLng: 'en',
+    compatibilityJSON: 'v3', // By default React Native projects does not support Intl
 
-  // allows integrating dynamic values into translations.
-  interpolation: {
-    escapeValue: false, // escape passed in values to avoid XSS injections
+    // allows integrating dynamic values into translations.
+    interpolation: {
+      escapeValue: false, // escape passed in values to avoid XSS injections
+    },
   },
-});
+  undefined,
+);
 
 export default i18n;
