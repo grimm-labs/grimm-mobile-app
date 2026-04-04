39c70642128778ec60aae154bf3fad3a2082fe26
nejos97 <nenbajonathan@protonmail.com>
2026-04-04 13:48:59 +0100

    feat: update app icon badge configuration to disable in CI environments

--- app.config.ts
+++ app.config.ts
@@ -5,7 +5,7 @@ import type { AppIconBadgeConfig } from 'app-icon-badge/types';
 import { ClientEnv, Env } from './env';
 
 const appIconBadgeConfig: AppIconBadgeConfig = {
-  enabled: Env.APP_ENV !== 'production',
+  enabled: Env.APP_ENV !== 'production' && !process.env.CI,
   badges: [
     {
       text: Env.APP_ENV,
